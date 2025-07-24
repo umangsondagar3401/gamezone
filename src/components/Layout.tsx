12ad045f27bb00654a38e7340cc379ca40065aff
Ayushi diyora <112619773+aayud22@users.noreply.github.com>
2025-07-24 16:44:36 +0530

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import SEO from "./SEO";
+import { getPageSeo } from "../lib/seoUtils";
+import { Outlet, useLocation } from "react-router-dom";
 
 const Layout: React.FC = () => {
+  const location = useLocation();
+  const { title, description, keywords } = getPageSeo(location.pathname);
+
   return (
-    <div className="flex items-center justify-center p-4 md:p-8 bg-gray-50 min-h-screen">
-      <main className="max-w-[1440px] w-full mx-auto">
-        <Outlet />
-      </main>
-    </div>
+    <>
+      <SEO title={title} description={description} keywords={keywords} />
+      <div className="flex items-center justify-center p-4 md:p-8 bg-gray-50 min-h-screen">
+        <main className="max-w-[1440px] w-full mx-auto">
+          <Outlet />
+        </main>
+      </div>
+    </>
   );
 };
 
